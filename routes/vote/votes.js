45210069a5859da9427ec43f4387459bf6fcc1d1
Will Colton <coltonw@gmail.com>
2014-04-28 22:43:51 -0400

    Added more validation to posting a vote.

--- routes/vote/votes.js
+++ routes/vote/votes.js
@@ -14,7 +14,8 @@ exports.post = {
             ballot = body.ballot,
             vote = body.vote,
             nickname = body.nickname;
-        if(validate.vote(vote)) {
+        if(validate.vote(vote) && typeof nickname === 'string' &&
+                validate.objectIdHexString(ballot)) {
             req.db.collection('vote', function(er, collection) {
                 collection.insert({
                     _id: id,
