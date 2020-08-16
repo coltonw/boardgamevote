40a7954c94af811bd125abbe212c3d1cc2dcb5c0
Will Colton <coltonw@gmail.com>
2020-08-16 15:07:19 -0400

    Fix some problems with the db migration

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
-var ballotRoute = require('./ballot/ballot_id/ballot.js');
+var ballotRoute = require("./ballot/ballot_id/ballot.js");
 
 /*
  * GET home page.
  */
-exports.index = function(req, res){
-    req.db.collection('ballot', function(er, collection) {
-        collection.findOne({},{sort: {$natural:-1}},function(err, ballot) {
-            if (!ballot) return next(new Error('Ballot not found'));
+exports.index = function (req, res) {
+    req.db.collection("ballot", function (er, collection) {
+        collection.findOne({}, { sort: { $natural: -1 } }, function (
+            err,
+            ballot
+        ) {
+            if (!ballot) return next(new Error("Ballot not found"));
             req.ballot = ballot;
             ballotRoute.vote(req, res);
         });
